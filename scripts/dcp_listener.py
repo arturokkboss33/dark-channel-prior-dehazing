b4b8aa704e739c6df71651ae74c3f1c491b793e7
Arturo Gomez Chavez <a.gomezchavez@jacobs-university.de>
2018-09-04 22:40:00 +0200

    Create ROS package and first node listener

--- scripts/dcp_listener.py
+++ scripts/dcp_listener.py
@@ -1,68 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import argparse
+import roslib
+roslib.load_manifest('dcp_uw_dehaze')
+import sys
+import rospy
+import rospkg
+import message_filters
+from sensor_msgs.msg import Image
 from functools import partial
-
-from PIL import Image
-
-from util import get_filenames
+from PIL import Image as PILImage
 from dehaze import dehaze
 
-SP_IDX = (5, 6, 8, 12)  # for testing parameters
-SP_PARAMS = ({'tmin': 0.2, 'Amax': 170, 'w': 15, 'r': 40},
-             {'tmin': 0.5, 'Amax': 190, 'w': 15, 'r': 40},
-             {'tmin': 0.5, 'Amax': 220, 'w': 15, 'r': 40})
+# ===CALLBACK TO PERFORM DEHAZING
 
 
-def generate_results(src, dest, generator):
-    print 'processing', src + '...'
-    im = Image.open(src)
-    dark, rawt, refinedt, rawrad, rerad = generator(im)
-    dark.save(dest % 'dark')
-    rawt.save(dest % 'rawt')
-    refinedt.save(dest % 'refinedt')
-    rawrad.save(dest % 'radiance-rawt')
-    rerad.save(dest % 'radiance-refinedt')
-    print 'saved', dest
+def dcp_callback(depth,stereo_image):
+    print("Got an image")
+
+# ===PUBLISHERS AND SUBSCRIBERS
 
 
-def main():
-    filenames = get_filenames()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=int,
-                        choices=range(len(filenames)),
-                        help="index for single input image")
-    parser.add_argument("-t", "--tmin", type=float, default=0.2,
-                        help="minimum transmission rate")
-    parser.add_argument("-A", "--Amax", type=int, default=220,
-                        help="maximum atmosphere light")
-    parser.add_argument("-w", "--window", type=int, default=15,
-                        help="window size of dark channel")
-    parser.add_argument("-r", "--radius", type=int, default=40,
-                        help="radius of guided filter")
+def init():
 
-    args = parser.parse_args()
+    depth_sub = message_filters.Subscriber('/depth_camera/depth/image_raw', Image)
+    image_sub = message_filters.Subscriber(
+        '/stereo_camera/right/image_uncomp', Image)
 
-    if args.input is not None:
-        src, dest = filenames[args.input]
-        dest = dest.replace("%s",
-                     "%s-%d-%d-%d-%d" % ("%s", args.tmin * 100, args.Amax,
-                                           args.window, args.radius))
-        generate_results(src, dest, partial(dehaze, tmin=args.tmin, Amax=args.Amax,
-                                           w=args.window, r=args.radius))
-    else:
-        for idx in SP_IDX:
-            src, dest = filenames[idx]
-            for param in SP_PARAMS:
-                newdest = dest.replace("%s",
-                     "%s-%d-%d-%d-%d" % ("%s", param['tmin'] * 100,
-                                         param['Amax'], param['w'],
-                                         param['r']))
-                generate_results(src, newdest, partial(dehaze, **param))
+    ts = message_filters.ApproximateTimeSynchronizer(
+        [depth_sub, image_sub], 10, 0.1)
+    ts.registerCallback(dcp_callback)
 
-        for src, dest in filenames:
-            generate_results(src, dest, dehaze)
 
+# ===MAIN
 if __name__ == '__main__':
-    main()
+    try:
+        rospy.init_node('dcp_listener')
+        init()
+
+    except rospy.ROSInterruptException:
+        pass
+
+    rospy.spin()
