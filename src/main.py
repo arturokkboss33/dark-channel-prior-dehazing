6a86c059001d1979f9588f1c7d9c4bc610b888ce
Arturo Gomez Chavez <a.gomezchavez@jacobs-university.de>
2018-10-17 19:10:59 +0200

    Modify argument parsing to receive filenames from command line

--- src/main.py
+++ src/main.py
@@ -2,37 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 from functools import partial
-
 from PIL import Image
-
-from util import get_filenames
 from dehaze import dehaze
 
-SP_IDX = (5, 6, 8, 12)  # for testing parameters
-SP_PARAMS = ({'tmin': 0.2, 'Amax': 170, 'w': 15, 'r': 40},
-             {'tmin': 0.5, 'Amax': 190, 'w': 15, 'r': 40},
-             {'tmin': 0.5, 'Amax': 220, 'w': 15, 'r': 40})
-
+def set_filenames(img_name):
+    """Return tuples for source and template destination"""
+    SRC_DIR = 'imgs'
+    DEST_DIR = 'results'
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    parent_dir, _ = os.path.split(file_dir)
+    src_path = os.path.join(parent_dir, SRC_DIR)
+    dest_path = os.path.join(parent_dir, DEST_DIR)
+    filenames = []
+    base, ext = os.path.splitext(img_name)
+    tempname = base + '_result'
+    src_file = os.path.join(src_path, img_name)
+    dest_file = os.path.join(dest_path, tempname)
+    
+    return src_file, dest_file
 
 def generate_results(src, dest, generator):
     print 'processing', src + '...'
     im = Image.open(src)
     dark, rawt, refinedt, rawrad, rerad = generator(im)
-    dark.save(dest % 'dark')
-    rawt.save(dest % 'rawt')
-    refinedt.save(dest % 'refinedt')
-    rawrad.save(dest % 'radiance-rawt')
-    rerad.save(dest % 'radiance-refinedt')
+    dark.save(dest + 'dark.png')
+    rawt.save(dest + 'rawt.png')
+    refinedt.save(dest + 'refinedt.png')
+    rawrad.save(dest + 'radiance-rawt.png')
+    rerad.save(dest + 'radiance-refinedt.png')
     print 'saved', dest
 
 
 def main():
-    filenames = get_filenames()
+    """Parse users arguments and apply DCP to each specified file."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=int,
-                        choices=range(len(filenames)),
-                        help="index for single input image")
+
+    parser.add_argument("-i", "--input", type=str,
+                        help="image filename")
     parser.add_argument("-t", "--tmin", type=float, default=0.2,
                         help="minimum transmission rate")
     parser.add_argument("-A", "--Amax", type=int, default=220,
@@ -44,25 +52,19 @@ def main():
 
     args = parser.parse_args()
 
+    ##>Loop through each file and apply DCP
     if args.input is not None:
-        src, dest = filenames[args.input]
-        dest = dest.replace("%s",
-                     "%s-%d-%d-%d-%d" % ("%s", args.tmin * 100, args.Amax,
-                                           args.window, args.radius))
-        generate_results(src, dest, partial(dehaze, tmin=args.tmin, Amax=args.Amax,
-                                           w=args.window, r=args.radius))
+        img_files = args.input.split(" ")
+        #print(img_files) #DEBUGGING LINE
+        for img in img_files:
+            src, dest = set_filenames(img)
+            #print(src);print(dest); #DEBUGGING LINE
+            dest = dest + ("_%d%s_%d_%d_%d" % (args.tmin * 100, "e-2", args.Amax, args.window, args.radius))
+            #print(dest) #DEBUGGING LINE
+            generate_results(src, dest, partial(dehaze, tmin=args.tmin, Amax=args.Amax, w=args.window, r=args.radius))
     else:
-        for idx in SP_IDX:
-            src, dest = filenames[idx]
-            for param in SP_PARAMS:
-                newdest = dest.replace("%s",
-                     "%s-%d-%d-%d-%d" % ("%s", param['tmin'] * 100,
-                                         param['Amax'], param['w'],
-                                         param['r']))
-                generate_results(src, newdest, partial(dehaze, **param))
-
-        for src, dest in filenames:
-            generate_results(src, dest, dehaze)
+        print("No input files were given")
+    
 
 if __name__ == '__main__':
     main()
