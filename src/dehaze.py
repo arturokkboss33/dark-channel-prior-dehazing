c0a9c79bcd1eb031f5d4a56dd2af113042a74ba5
Arturo Gomez Chavez <a.gomezchavez@jacobs-university.de>
2018-10-18 20:16:57 +0200

    Format identation to spaces not tabs

--- src/dehaze.py
+++ src/dehaze.py
@@ -34,10 +34,10 @@ def get_dark_channel(I, w):
     M, N, _ = I.shape
     padded = np.pad(I, ((w / 2, w / 2), (w / 2, w / 2), (0, 0)), 'edge')
     dark_ch = np.zeros((M, N))
-    ##NOTE:Choose the minimum pixel value in the window
+    # NOTE:Choose the minimum pixel value in the window
     for i, j in np.ndindex(dark_ch.shape):
-        ##NOTE:If no axis is given for he minimum, the array is flattened
-        ##NOTE: CVPR09, eq.5
+        # NOTE:If no axis is given for he minimum, the array is flattened
+        # NOTE: CVPR09, eq.5
         dark_ch[i, j] = np.min(padded[i:i + w, j:j + w, :])
     return dark_ch
 
@@ -55,26 +55,26 @@ def get_atmosphere(I, dark_ch, p, depth_img=None):
     -----------
     A 3-element array containing atmosphere light ([0, max_color_val-1]) for each channel
     """
-    ##NOTE:If depth is given, copmute the average of pixels values in the horizon
-    ##else follow the normal DCP implementation
+    # NOTE:If depth is given, copmute the average of pixels values in the horizon
+    # else follow the normal DCP implementation
     if depth_img is not None:
         M, N = depth_img.shape
         flat_depth = depth_img.ravel()
         flat_depth = np.nan_to_num(flat_depth)*255.
         flat_depth = np.maximum(np.minimum(flat_depth, 255.), 0.0001)/255.
-        flat_img = I.reshape(M*N,3)
+        flat_img = I.reshape(M*N, 3)
 
         return np.average(flat_img, axis=0, weights=flat_depth)
     else:
-        ##NOTE: CVPR09, eq. 4.4
+        # NOTE: CVPR09, eq. 4.4
         M, N = dark_ch.shape
         flat_I = I.reshape(M * N, 3)
         flat_dark = dark_ch.ravel()
-        ##NOTE: Find top M * N * p indexes
+        # NOTE: Find top M * N * p indexes
         search_idx = (-flat_dark).argsort()[:int(M * N * p)]
-        #print 'atmosphere light region:', [(i / N, i % N) for i in search_idx] #DEBUGGING LINE
+        # print 'atmosphere light region:', [(i / N, i % N) for i in search_idx] #DEBUGGING LINE
 
-        ##NOTE: Return the highest intensity for each channel
+        # NOTE: Return the highest intensity for each channel
         return np.max(flat_I.take(search_idx, axis=0), axis=0)
 
 
@@ -119,44 +119,47 @@ def dehaze_raw(I, t_min=0.2, atm_max=220, w=15, p=0.0001,
     -----------
     (Idark, A, rawt, refinedt) if guided=False, then rawt == refinedt
     """
-    
-    ##NOTE:Check if depth image was provided
+
+    # NOTE:Check if depth image was provided
     flag_use_depth = False
     if depth_img is not None:
         flag_use_depth = True
 
-    ##NOTE:First, get dark channel image
-    ##NOTE:Change color space if dealing with underwater images
-    Iprime = np.zeros(I.shape,dtype=np.float64)
+    # NOTE:First, get dark channel image
+    # NOTE:Change color space if dealing with underwater images
+    Iprime = np.zeros(I.shape, dtype=np.float64)
     if flag_uw:
-        Iprime[:,:,0] = 255. - I[:,:,0]
-        Iprime[:,:,1] = 255. - I[:,:,1]
-        Iprime[:,:,2] = I[:,:,2]
+        Iprime[:, :, 0] = 255. - I[:, :, 0]
+        Iprime[:, :, 1] = 255. - I[:, :, 1]
+        Iprime[:, :, 2] = I[:, :, 2]
     else:
         Iprime = I
 
     Idark = [] if flag_use_depth else get_dark_channel(Iprime, w)
 
-    ##NOTE:Estimate the atmospheric light, this is done always with the original image
+    # NOTE:Estimate the atmospheric light, this is done always with the original image
     atm_light = get_atmosphere(I, Idark, p, depth_img)
     atm_light = np.minimum(atm_light, atm_max)
     print('atmosphere', atm_light)
 
-    ##NOTE:Estimate transmission image which correlates to depth
+    # NOTE:Estimate transmission image which correlates to depth
     if flag_use_depth:
-        M,N,_ = I.shape
-        white = np.full_like(np.zeros((M,N),dtype=np.float64), max_color_val - 1)
+        M, N, _ = I.shape
+        white = np.full_like(
+            np.zeros((M, N), dtype=np.float64), max_color_val - 1)
         return white, atm_light, white, white
     else:
         rawt = get_transmission(Iprime, atm_light, omega, w)
-        print('raw transmission rate between [%.4f, %.4f]' % (rawt.min(), rawt.max()))
-        ##NOTE: Refine transmission rate through guided filter (edge-preserving filter)
+        print('raw transmission rate between [%.4f, %.4f]' % (
+            rawt.min(), rawt.max()))
+        # NOTE: Refine transmission rate through guided filter (edge-preserving filter)
         rawt = refinedt = np.maximum(rawt, t_min)
         if guided:
             normI = (I - I.min()) / (I.max() - I.min())
             refinedt = guided_filter(normI, refinedt, r, eps)
 
-        print('refined transmission rate between [%.4f, %.4f]' % (refinedt.min(), refinedt.max()))
+        print('refined transmission rate between [%.4f, %.4f]' % (
+            refinedt.min(), refinedt.max()))
 
         return Idark, atm_light, rawt, refinedt
 
@@ -177,9 +180,9 @@ def get_radiance(I, atm_light, trans, depth_img=None):
     M * N * 3 numpy array for the recovered radiance
     """
 
-    ##NOTE: Tile transmission image
+    # NOTE: Tile transmission image
     tiledt = np.zeros_like(I)
-    attenuation_coeff = np.array([0.5,0.5,0.5])
+    attenuation_coeff = np.array([0.5, 0.5, 0.5])
     if depth_img is not None:
         transmission_B = np.exp(-attenuation_coeff[0]*depth_img)
         transmission_G = np.exp(-attenuation_coeff[1]*depth_img)
@@ -190,15 +193,16 @@ def get_radiance(I, atm_light, trans, depth_img=None):
     else:
         tiledt[:, :, R] = tiledt[:, :, G] = tiledt[:, :, B] = trans
 
-    ##NOTE: CVPR09, eq.16
+    # NOTE: CVPR09, eq.16
     return (I - atm_light) / tiledt + atm_light
 
 
 def to_img(raw):
-    ##NOTE: Threshold image to be in the range 0 - max_color_val-1
+    # NOTE: Threshold image to be in the range 0 - max_color_val-1
     cut = np.maximum(np.minimum(raw, max_color_val - 1), 0).astype(np.uint8)
 
     return Image.fromarray(cut)
+
 
 def dehaze(img, t_min=0.2, atm_max=220, w=15, p=0.0001,
            omega=0.95, guided=True, r=40, eps=1e-3, flag_uw=False, depth_img=None):
@@ -219,16 +223,16 @@ def dehaze(img, t_min=0.2, atm_max=220, w=15, p=0.0001,
     refiend transmission estimate, recovered radiance with raw t,
     recovered radiance with refined t.
     """
-    
+
     I = np.asarray(img, dtype=np.float64)
     Idepth = None
     if depth_img is not None:
         depth_img = depth_img.convert("L")
         Idepth = np.asarray(depth_img, dtype=np.float64)
     Idark, atm_light, rawt, refinedt = dehaze_raw(I, t_min, atm_max, w, p,
-                                          omega, guided, r, eps, flag_uw, Idepth)
-    M,N,_ = I.shape
-    white = np.full_like(np.zeros((M,N),dtype=np.float64), max_color_val - 1)
+                                                  omega, guided, r, eps, flag_uw, Idepth)
+    M, N, _ = I.shape
+    white = np.full_like(np.zeros((M, N), dtype=np.float64), max_color_val - 1)
 
     return [to_img(raw) for raw in (Idark, white * rawt, white * refinedt,
                                     get_radiance(I, atm_light, rawt, Idepth),
--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from PIL import Image
 from dehaze import dehaze
 import yaml
 
-def set_filenames(img_name,depth_img=None):
+
+def set_filenames(img_name, depth_img=None):
     """Return tuples for source and template destination"""
     SRC_DIR = 'imgs'
     DEST_DIR = 'results'
@@ -24,8 +25,9 @@ def set_filenames(img_name,depth_img=None):
     if depth_img is None:
         return src_file, dest_file
     else:
-        depth_file = os.path.join(src_path,depth_img)
+        depth_file = os.path.join(src_path, depth_img)
         return src_file, dest_file, depth_file
+
 
 def generate_results(src, dest, generator, depth_img_src=None):
     print('processing', src + '...')
@@ -33,13 +35,14 @@ def generate_results(src, dest, generator, depth_img_src=None):
     depth_img = None
     if depth_img_src is not None:
         depth_img = Image.open(depth_img_src)
-    
-    dark, raw_trans, refined_trans, raw_rad, refined_rad = generator(img,depth_img=depth_img)
+
+    dark, raw_trans, refined_trans, raw_rad, refined_rad = generator(
+        img, depth_img=depth_img)
     if depth_img_src is None:
         dark.save(dest + '_dark-channel.png')
         raw_trans.save(dest + '_raw-transmission.png')
         refined_trans.save(dest + '_refined-transmission.png')
-    
+
     raw_rad.save(dest + '_raw-radiance.png')
     refined_rad.save(dest + '_refined-radiance.png')
     print('saved', dest)
@@ -54,41 +57,43 @@ def main():
 
     args = parser.parse_args()
 
-    ##NOTE:Read config file with all params
+    # NOTE:Read config file with all params
     file_dir = os.path.dirname(os.path.realpath(__file__))
-    config_file = os.path.join(os.path.split(file_dir)[0],'config/'+args.config)
-    config_dict = yaml.safe_load(open(config_file,'r'))
-    #print(config_dict['filter_radius']) #DEBUGGING LINE
+    config_file = os.path.join(os.path.split(
+        file_dir)[0], 'config/'+args.config)
+    config_dict = yaml.safe_load(open(config_file, 'r'))
+    # print(config_dict['filter_radius']) #DEBUGGING LINE
 
-    ##NOTE:Loop through each file and apply DCP
+    # NOTE:Loop through each file and apply DCP
     if config_dict['input']:
         img_files = config_dict['input'].split(" ")
-        #print(img_files) #DEBUGGING LINE
+        # print(img_files) #DEBUGGING LINE
 
-        ##NOTE:Check if a depth image was given
+        # NOTE:Check if a depth image was given
         flag_use_depth = False
         depth_img_src = None
         depth_files = []
         if config_dict['depth_image']:
             depth_files = config_dict['depth_image'].split(" ")
             flag_use_depth = True
-        for img_idx,img in enumerate(img_files):
+        for img_idx, img in enumerate(img_files):
             if flag_use_depth is False:
                 src, dest = set_filenames(img)
-                #print(src);print(dest) #DEBUGGING LINE
+                # print(src);print(dest) #DEBUGGING LINE
             else:
-                src, dest, depth_img_src = set_filenames(img,depth_files[img_idx])
-                #print(src);print(dest);print(depth_img_src) #DEBUGGING LINE
+                src, dest, depth_img_src = set_filenames(
+                    img, depth_files[img_idx])
+                # print(src);print(dest);print(depth_img_src) #DEBUGGING LINE
 
-            dest = dest + ("_%d%s_%d_%d_%d" % (config_dict['min_transmission'] * 100, "e-2", 
-                                                config_dict['max_atm_light'], config_dict['window'], config_dict['filter_radius']))
-            #print(dest) #DEBUGGING LINE
-            generate_results(src, dest, partial(dehaze, t_min=config_dict['min_transmission'], atm_max=config_dict['max_atm_light'], 
-                                                w=config_dict['window'], r=config_dict['filter_radius'], 
+            dest = dest + ("_%d%s_%d_%d_%d" % (config_dict['min_transmission'] * 100, "e-2",
+                                               config_dict['max_atm_light'], config_dict['window'], config_dict['filter_radius']))
+            # print(dest) #DEBUGGING LINE
+            generate_results(src, dest, partial(dehaze, t_min=config_dict['min_transmission'], atm_max=config_dict['max_atm_light'],
+                                                w=config_dict['window'], r=config_dict['filter_radius'],
                                                 flag_uw=config_dict['underwater_dehaze']), depth_img_src)
     else:
         print("No input files were given")
-    
+
 
 if __name__ == '__main__':
     main()
